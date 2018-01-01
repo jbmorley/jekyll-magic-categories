a4ff0c2d795c48098a77c73d791372a2b5e5fcbb
Jason Barrie Morley <jason.morley@inseven.co.uk>
2018-01-01 11:36:24 +0100

    Using the relative path.

--- magic-categories.rb
+++ magic-categories.rb
@@ -6,7 +6,7 @@ module Reading
 
 		def generate(site)
 			for post in site.posts.docs
-				categories = File.dirname(post.path).split("/").drop(1).map{ |dirname| dirname.split("#")[0] }
+				categories = File.dirname(post.relative_path).split("/").drop(1).map{ |dirname| dirname.split("#")[0] }
 				post.data['categories'] |= categories
 			end
 
