8f1424cf8b1841568f91479868eef1c37d01b8a6
Jason Barrie Morley <jason.morley@inseven.co.uk>
2018-01-01 10:46:36 +0100

    Compatibility fix.

--- magic-categories.rb
+++ magic-categories.rb
@@ -5,9 +5,9 @@ module Reading
 	class Generator < Jekyll::Generator
 
 		def generate(site)
-			for post in site.posts
+			for post in site.posts.docs
 				categories = File.dirname(post.path).split("/").drop(1).map{ |dirname| dirname.split("#")[0] }
-				post.categories |= categories
+				post.data['categories'] |= categories
 			end
 
 		end
